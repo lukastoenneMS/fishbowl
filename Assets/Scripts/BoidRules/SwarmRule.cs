5ebda60a345b33ebe92f2827c9f25d47b9b3ba9a
Lukas Tönne <lutnne@microsoft.com>
2019-04-17 17:41:16 +0100

    Testing modified swarm behavior.

--- Assets/Scripts/BoidRules/SwarmRule.cs
+++ Assets/Scripts/BoidRules/SwarmRule.cs
@@ -50,7 +50,10 @@ namespace Boids
             context.Query.Radius(context.Tree, state.position, maxRadius, queryResults);
 
             float totweight = 0.0f;
-            Vector3 dir = Vector3.zero;
+            Vector3 pos_mean = Vector3.zero;
+            float pos_var = 0.0f;
+            Vector3 dir_mean = Vector3.zero;
+            float speed_mean = 0.0f;
             foreach (int idx in queryResults)
             {
                 // Skip own point
@@ -61,46 +64,57 @@ namespace Boids
 
                 BoidState queryState = context.States[idx];
                 Vector3 delta = queryState.position - state.position;
-                float dist = delta.magnitude;
+                float weight = GetVisibility(state, queryState.position);
 
-                float swarmWeight = Mathf.Clamp(1.0f - (dist - minRadius) / deltaRadius, 0.0f, 1.0f);
-                float followWeight = Mathf.Clamp(1.0f - dist / maxRadius, 0.0f, 1.0f);
-                if (forwardAsymmetry > 0.0f)
-                {
-                    float fwd = Mathf.Max(0.0f, 0.5f + 0.5f * Vector3.Dot(delta.normalized, state.direction));
-                    float fwdFactor = 1.0f - (1.0f - fwd) * forwardAsymmetry;
-                    swarmWeight *= fwdFactor;
-                    followWeight *= fwdFactor;
-                }
-
-                // TODO arbitrary blending between goal behavior and direction alignment
-                // float dirGoalBlend = Mathf.SmoothStep(0.0f, 1.0f, dist / maxRadius);
-
-                // dir += queryState.direction * swarmWeight * (1.0f - dirGoalBlend);
-                // dir += delta * followWeight * dirGoalBlend;
-                dir += queryState.direction * swarmWeight;
-                dir += delta * followWeight;
-
-                if (dbg != null)
-                {
-                    dbg.AddSwarmPoint(queryState.position, swarmWeight);
-                }
-
-                totweight += swarmWeight + followWeight;
+                totweight += weight;
+                pos_mean += delta * weight;
+                pos_var += delta.sqrMagnitude * weight;
+                dir_mean += queryState.direction * weight;
+                speed_mean += Vector3.Dot(queryState.velocity, queryState.direction) * weight;
             }
+
             if (totweight > 0.0f)
             {
-                dir /= totweight;
-                target = new BoidTarget(dir, boid.Settings.MaxAcceleration);
+                float norm = 1.0f / totweight;
+                pos_mean *= norm;
+                pos_var = pos_var * norm - pos_mean.sqrMagnitude;
+                dir_mean *= norm;
+
+                // Adjust direction when swimming against the flow
+                // Otherwise gradually increase swarm behavior, i.e. swim to the center of the swarm
+                float followFactor = Mathf.SmoothStep(1.0f, 0.0f, 0.5f + 0.5f * Vector3.Dot(state.direction, dir_mean.normalized));
+
+                // TODO scale such that follow factor is 1 when variance gets smaller than threshold
+                // float swarmSizeFactor = 1.0f / Mathf.Sqrt(2.0f * Mathf.PI * pos_var);
+                // float swarmSizeFactor = 1.0f;
+                // followFactor = Mathf.Min(swarmSizeFactor * Mathf.Exp(-0.5f * (pos_mean - state.position).sqrMagnitude / pos_var), 1.0f);
+
+                Vector3 dir = (pos_mean - state.position).normalized * (1.0f - followFactor) + dir_mean.normalized * followFactor;
+                // float speed = boid.Settings.MaxSpeed * (1.0f - followFactor) + speed_mean * followFactor;
+                float speed = boid.Settings.MaxSpeed * (1.0f - followFactor);
+                target = new BoidTarget(dir, speed);
                 priority = PriorityMedium;
-                return true;
             }
-            else
+
+            target = null;
+            priority = PriorityNone;
+            return false;
+        }
+
+        private float GetVisibility(BoidState state, Vector3 point)
+        {
+            // TODO arbitrary function to increase weight of forward targets
+            Vector3 delta = point - state.position;
+            float vis = 0.5f * 0.5f * Vector3.Dot(delta.normalized, state.direction);
+            vis = 0.3f + 0.7f * vis;
+
+            // Distance falloff
+            if (minRadius < maxRadius)
             {
-                target = null;
-                priority = PriorityNone;
-                return false;
+                vis *= Mathf.SmoothStep(1.0f, 0.0f, (delta.magnitude - minRadius) / (maxRadius - minRadius));
             }
+
+            return vis;
         }
     }
 }
